004c142e007bf749942c2d1e7b5ae257682a1856
Abhishek Nath <abhisheknath1042@gmail.com>
2022-09-26 08:07:33 +0530

    Dynamic Array

---
+++ C++ Concepts/dynamicArray.cpp
@@ -0,0 +1,33 @@
+// default
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+
+	int size;
+	cout << "Enter Size: " ;
+	cin >> size;
+
+	int *myArray = new int[size];
+	
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Array[" << i << "] ";
+		cin >> myArray[i];
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(myArray+i) << " ";
+	}
+
+	delete[]myArray;
+	myArray=NULL;
+	
+	
+
+	return 0;
+}
